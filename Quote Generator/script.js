b6a74dd6a2190b1d866dca14773f02805351c640
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-01-07 02:26:01 -0500

    ui correction

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,4 +1,3 @@
-
 let quoteText = document.getElementById("quote");
 
 let authorText = document.getElementById("author");
@@ -6,27 +5,34 @@ let authorText = document.getElementById("author");
 let apiQuotes = [];
 
 const randomQuote = () => {
-
   const newQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // if author is unknown
+  if (newQuote.author) {
+    authorText.textContent = newQuote.author;
+  } else {
+    authorText.textContent = "Unknown";
+  }
 
-  authorText.textContent = newQuote.author;
+  //if the quote is long
+
+  if (newQuote.text.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
   quoteText.textContent = newQuote.text;
- 
 };
-
 
 // get quotes from API
 
 const getQuotes = async () => {
-
   const api_url = "https://type.fit/api/quotes";
 
   try {
-
     const response = await fetch(api_url);
     apiQuotes = await response.json();
     randomQuote();
-
   } catch (error) {
     response.status(500).json({ status: "Error", msg: error.message });
   }
