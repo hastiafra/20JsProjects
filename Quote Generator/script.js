83a12d8233b4667fa4179187f7863bbd3ca89140
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-01-07 02:01:52 -0500

    updating ui tex

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,19 +1,35 @@
+
+let quoteText = document.getElementById("quote");
+
+let authorText = document.getElementById("author");
+
 let apiQuotes = [];
 
 const randomQuote = () => {
-  const quote = apiQuotes[Math.floor(Math.random()) * apiQuotes.length];
-  console.log(quote)
+
+  const newQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  authorText.textContent = newQuote.author;
+  quoteText.textContent = newQuote.text;
+ 
 };
+
+
 // get quotes from API
 
 const getQuotes = async () => {
+
   const api_url = "https://type.fit/api/quotes";
 
   try {
+
     const response = await fetch(api_url);
     apiQuotes = await response.json();
     randomQuote();
-  } catch (error) {}
+
+  } catch (error) {
+    response.status(500).json({ status: "Error", msg: error.message });
+  }
 };
 
 getQuotes();
