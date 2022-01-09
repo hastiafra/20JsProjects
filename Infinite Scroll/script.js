4dbc9e078a3c0e7b6e765d1fd2de5b01b0513059
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-01-09 15:08:23 -0500

    api

--- Infinite Scroll/script.js
+++ Infinite Scroll/script.js
@@ -1,2 +1,16 @@
 
+require("dotenv").config();
+const My_key = process.env.apiUrl;
 
+const getPhotos = async () => {
+
+  try {
+    const response = await fetch(My_key);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    response.status(500).json({ status: "Error", msg: error.message });
+  }
+};
+
+getPhotos();
