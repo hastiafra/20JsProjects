52b809350c6799a3fd2123647fbbc0ab340a1a54
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-01-09 22:36:49 -0500

    fetching img

--- Infinite Scroll/README.md
+++ Infinite Scroll/README.md
@@ -1,1 +1,3 @@
 Infinit Scroll
+
+Fetching the data in JSON format from the Unsplash Api. API is limited to 50 requests per hour. 
--- Infinite Scroll/script.js
+++ Infinite Scroll/script.js
@@ -1,16 +1,48 @@
 import {APIKEY} from "./env.js";
 
-console.log(APIKEY)
+const container = document.getElementById("img-container");
+
+const loader = document.getElementById("loader");
+
+let photosArr=[];
+
+
+
 
 const getPhotos = async () => {
-
+  
   try {
-    const response = await fetch(My_key);
-    const data = await response.json();
-    console.log(data);
-  } catch (err) {
-    response.status(500).json({ status: "Error", msg: error.message });
+    const response = await fetch(APIKEY);
+    photosArr = await response.json();
+    // console.log(photosArr);
+    displayPhotos();
+
+  } catch (error) {
+    console.log(error);
+  
   }
 };
 
+const displayPhotos = () =>{
+  console.log(photosArr);
+
+  photosArr.forEach((photo)=>{
+
+    const item = document.createElement("a");
+
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target","_blank");
+    let img = document.createElement("img");
+
+    img.src= `${photo.urls.regular}`;
+    img.setAttribute("alt",photo.alt_description);
+    img.setAttribute("title",photo.alt_description);
+  
+    item.appendChild(img);
+    container.appendChild(item);
+
+
+
+  })
+}
 getPhotos();
